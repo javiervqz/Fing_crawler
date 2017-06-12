9f5eb9a941a35d6e97dc95e374e70de824dfd91e
Javier Vazquez <jvrvzqzp@gmail.com>
2017-06-12 13:15:37 -0600

    better parsing without BeSoup

--- fing_prof.py
+++ fing_prof.py
@@ -1,74 +1,30 @@
-import urllib, re
+import urllib
 import HTMLParser
 from BeautifulSoup import *
+from lxml.html import parse
+from pandas.io.parsers import TextParser
 
-
-url_dir= 'http://fing.uach.mx/facultad/2015/09/24/directorio_docente/'
-url_main = 'http://fing.uach.mx'
-html = urllib.urlopen(url_dir).read()
-urlText = []
 
 class parseText(HTMLParser.HTMLParser):
 	def handle_data(self, data):
 		if data != '\n':
 			urlText.append(data)
 
-lParser = parseText()
-lParser.feed(html)
-lParser.close()
+def _unpack (row, kind = 'td'):
+	elts = row.findall('.//%s' % kind)
+	return [val.text for val in elts]
 
+def parse_options_data (table):
+	rows = table.findall('.//tr')
+	header = _unpack(rows[0], kind = 'th')
+	data = [_unpack(r) for r in rows[1:]]
+	return TextParser(data, names=header).get_chunk()
 
+url_dir= 'http://fing.uach.mx/facultad/2015/09/24/directorio_docente/'
+url_main = 'http://fing.uach.mx'
+parsed = parse(url_dir)
+doc = parsed.getroot()
+links = doc.findall('.//a')
 
-
-# soup_dir = BeautifulSoup(html)
-# tags = soup_dir('a')
-#
-#
-# most_prof = []
-# for tag in tags:
-# 	url = tag.get('href')
-# 	try:
-# 		num = re.findall('[0-9]{4}', url)
-# 		if url.startswith('/facultad') and int(num[0]) > 2014:
-# 			most_prof.append(url)
-# 	except:
-# 		continue
-#
-# file = open("divtest.txt", "w")
-# for prof in most_prof:
-# 	url_prof = url_main + str(prof)
-# 	html_prof = urllib.urlopen(url_prof).read()
-# 	soup_prof = BeautifulSoup(html_prof)
-# 	tags1 = soup_prof("li")
-# 	tags2 = soup_prof("div")
-# 	tags3 = soup_prof("a")
-# 	list_prof = []
-# 	list_mat = []
-# 	for tag in tags2:
-# 		finder = re.compile(r'\bInteligencia\b| \balgoritmos\b | \bcomputadora\b', flags = re.I | re.X)
-# 		subjects = finder.findall(str(tag))
-# 		if subjects:
-# 			file.write(str(prof) + '\n' + str(tag) + '\n'+'\n')
-#
-# file.close()
-
-
-# tags = suop_prof('li')
-# for tag in tags:
-# 	print tag
-
-#i = 0
-#pcount = raw_input('Enter count: ')
-#position = raw_input('Enter position: ')
-#count = int(pcount)
-#while i < count:
-#	names = list()
-#	for tag in tags:
-#		url1 = tag.get('href', None)
-#		names.append(str(url1))
-#	url = names[int(position)-1]
-#	html = urllib.urlopen(url).read()
-#	soup = BeautifulSoup(html)
-#	tags = soup('a')
-#	i += 1
-#	print  names[int(position)-1]
+for link in links:
+	print link.text_content()
