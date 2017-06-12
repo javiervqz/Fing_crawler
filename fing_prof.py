2f9f45cf0097661f23f3d8a8ba93f63b5cd73f91
Javier Vazquez <jvrvzqzp@gmail.com>
2017-06-12 13:56:49 -0600

    Now i get only proffesors, big improvement

--- fing_prof.py
+++ fing_prof.py
@@ -1,30 +1,12 @@
 import urllib
-import HTMLParser
-from BeautifulSoup import *
 from lxml.html import parse
-from pandas.io.parsers import TextParser
 
-
-class parseText(HTMLParser.HTMLParser):
-	def handle_data(self, data):
-		if data != '\n':
-			urlText.append(data)
-
-def _unpack (row, kind = 'td'):
-	elts = row.findall('.//%s' % kind)
-	return [val.text for val in elts]
-
-def parse_options_data (table):
-	rows = table.findall('.//tr')
-	header = _unpack(rows[0], kind = 'th')
-	data = [_unpack(r) for r in rows[1:]]
-	return TextParser(data, names=header).get_chunk()
 
 url_dir= 'http://fing.uach.mx/facultad/2015/09/24/directorio_docente/'
 url_main = 'http://fing.uach.mx'
 parsed = parse(url_dir)
 doc = parsed.getroot()
-links = doc.findall('.//a')
-
+links = doc.findall('.//div[@id="listalinks"]/p/a')
 for link in links:
 	print link.text_content()
+	print link.get("href")
