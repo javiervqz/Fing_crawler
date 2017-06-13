bd1cd3f63f1e76bf67ed20256425dbfdb6c10215
Javier Vazquez <jvrvzqzp@gmail.com>
2017-06-12 19:39:56 -0600

    Can't match courses with professor yet

--- fing_prof.py
+++ fing_prof.py
@@ -1,5 +1,6 @@
-import urllib
-from lxml.html import parse
+import urllib, re
+from lxml.html import parse, etree
+
 
 
 url_dir= 'http://fing.uach.mx/facultad/2015/09/24/directorio_docente/'
@@ -8,5 +9,17 @@ parsed = parse(url_dir)
 doc = parsed.getroot()
 links = doc.findall('.//div[@id="listalinks"]/p/a')
 for link in links:
-	print link.text_content()
-	print link.get("href")
+	prof_name = link.text_content()
+	profurl = url_main+link.get("href")
+	parsed = parse(profurl)
+	doc = parsed.getroot()
+
+	raw = doc.findall('.//div/h4')
+	finder = re.compile(r'\bCursos\b | \bImpartidos\b', flags = re.I | re.X)
+	for tag in raw:
+		aver = finder.findall(tag.text_content())
+		if aver:
+			print prof_name, tag
+		# if aver:
+		# 	header = tag.getprevious()
+		# 	print header, prof_name
