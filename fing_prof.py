8146e7a8d3fc31a0994dc763190c91684b3b5fa9
Javier Vazquez <jvrvzqzp@gmail.com>
2017-06-21 22:48:33 -0600

    Getting a match on source code

--- fing_prof.py
+++ fing_prof.py
@@ -1,9 +1,6 @@
-import urllib
+import urllib, sys,re,os
 from lxml.html import parse
 import bs4 as bs
-import sys
-import re
-import os
 
 
 
@@ -17,23 +14,21 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 i = 1.0
-finder = re.compile(r'\bInteligencia\b| \balgoritmos\b | \bcomputadora\b', flags = re.I | re.X)
-subjects = finder.findall(str(tag))
+finder = re.compile(r'\bInteligencia artificial\b| \balgoritmos\b | \bcomputadora\b | \bSoftware de bajo nivel\b', flags = re.I | re.X )
 for link in links:
 	prof_name = link.text_content()
 	completeName = os.path.join(output_dir, prof_name+".html")
-	file = open(completeName, "w")
 	profurl = url_main+link.get("href")
 	html = urllib.urlopen(profurl).read()
 	soup_prof = bs.BeautifulSoup(html, 'lxml')
-
-	for info in soup_prof.findAll("div", attrs ={'id' : None, 'align': None, 'class': None}):
-		if info is not None:
+	for info in soup_prof.findAll("div", attrs ={'id' : "secciones", 'align': None, 'class': None}):
+		if info is not None and finder.findall(str(info)):
+			file = open(completeName, "w")
 			#file.write(info.text.encode('utf-8') + '\n')
 			file.write(str(info) + ' \n' )
+			file.close()
 	a = (i/len(links))*100
-	print 'Writing file ' "%.2f" % a, '%'
+	print 'Finding matching professors ' "%.2f" % a, '%'
 	sys.stdout.write("\033[F") # Cursor up one line
 	i += 1
-	file.close()
 print '\n Done!'
